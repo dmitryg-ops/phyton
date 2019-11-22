6b8b47c3849dd41077389784d002be4d9830dfdf
Dmitry Gurvich <info@allit.lv>
2019-11-22 14:21:11 +0200

    5 DZ

--- lesson01/5_hw.py
+++ lesson01/5_hw.py
@@ -5,3 +5,27 @@
 Если фирма отработала с прибылью, вычислите рентабельность выручки (соотношение прибыли к выручке).
 Далее запросите численность сотрудников фирмы и определите прибыль фирмы в расчете на одного сотрудника.
 """
+
+viruchka = int(input('Введите выручку: '))
+izdershki = int(input('Введите издержки: '))
+sotrudniki = int(input('Введите количество сотрудников:  '))
+#rasxodi = int(input('Введите расходы: '))
+
+pribil = viruchka - izdershki
+print(pribil)
+if pribil > 0:
+    print(f'Работаем {pribil} в плюсе !!!')
+    rent = str(pribil / izdershki * 100) + '%'
+    print(f'Рентабильность = {rent} ')
+if sotrudniki > 0:
+    result = pribil / sotrudniki
+    print(f'Каждый сотрудник зарабатывает: {result}')
+
+
+else:
+    print(f'Работаем {pribil} в минус !!!')
+
+
+
+
+
